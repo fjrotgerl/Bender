58a201fd1f5926c10054b6500d6c0658d1636d0a
fjrotgerl <fjrotgerl@gmail.com>
2017-03-26 18:25:19 +0200

    2.0 sin comentar v.1

--- src/Bender.java
+++ src/Bender.java
@@ -6,61 +6,79 @@ import java.util.Arrays;
 public class Bender {
     String mapa;
 
+    // En aquesta funció cridam al String actual que anem a utilitzar
     public Bender(String mapa) {
         this.mapa = mapa;
     }
 
+    // En la funció run ens encarregam bàsicament de mirar el caràcter que tenim en la posició actual
     public String run() {
+        // Cream un StringBuilder per ficar els caràcters corresponents a la seva direcció
         StringBuilder sb = new StringBuilder();
+        // Cream dos arrays
+        // El primer array serà de 4 posicions i indicarà la direcció que hem de seguir
         char[] normal = new char[] {'S','E','N','W'};
+        // El segon array també serà de 4 posicions
+        // Aquest indicarà la direcció que hem de seguir però a la inversa
         char[] inverted = new char[] {'N','W','S','E'};
+        // Aquest array emmagatzemarà la direcció correcta segons un booleà
         char[] movement;
+        // Construim un array bidimensional del mapa
+        // Utilitzam un String
         char[][] mapaBidimensional = buildArray(this.mapa);
+        // Emmagatzemam en un array d'integers de dos posicions la posició inicial (la 'X')
         int[] position = startPosition(mapaBidimensional);
+        // Emmagatzemam en un array de 4 posicions les posicions de les dos 'T'
         int[] tPosition = transportPosition(mapaBidimensional);
+        // Cream un comptador que indicarà quan ha de canviar a la següent posició
         int cont = 0;
-        boolean perimetro = false;
+        // Cream 2 booleans
+        // collision s'encarrega de tornar el comptador a 0
+        // o també de aumentar el comptador
+        boolean collision = false;
+        // inverter s'encarrega de assignar al array movement les direccions correctes
+        // depenent de si hi ha una 'I' o no
         boolean inverter = false;
 
         while (true) {
-            if (inverter) { movement = inverted; }
+            // Primer de tot en el bucle assignam les direccions correctes segons el boolea inverter
+            if (inverter == true) { movement = inverted; }
             else { movement = normal; }
+            // Cream un array de dos posicions que s'encarregarà de moure per el mapa
+            int[] actual = movePosition(position, movement[cont]);
 
-            int[] aux = movePosition(position, movement[cont]);
-            if (mapaBidimensional[aux[0]][aux[1]] == ' ' || mapaBidimensional[aux[0]][aux[1]] == 'X') {
-                position = aux;
-                perimetro = false;
+            if (mapaBidimensional[actual[0]][actual[1]] == ' ' || mapaBidimensional[actual[0]][actual[1]] == 'X') {
+                collision = false;
+                position = actual;
                 sb.append(movement[cont]);
                 continue;
-            } if (mapaBidimensional[aux[0]][aux[1]] == '#') {
-                if (perimetro) { cont++; }
+            } if (mapaBidimensional[actual[0]][actual[1]] == '#') {
+                if (collision == true) { cont++; }
                 else {
+                    collision = true;
                     cont = 0;
-                    perimetro = true;
                 }
                 continue;
-            } if (mapaBidimensional[aux[0]][aux[1]] == 'T') {
-                if (aux[0] == tPosition[0] && aux[1] == tPosition[1] ) {
+            } if (mapaBidimensional[actual[0]][actual[1]] == 'T') {
+                if (actual[0] == tPosition[0] && actual[1] == tPosition[1] ) {
                     position[0] = tPosition[2];
                     position[1] = tPosition[3];
                 } else {
                     position[0] = tPosition[0];
                     position[1] = tPosition[1];
                 }
-                perimetro = false;
+                collision = false;
                 sb.append(movement[cont]);
                 continue;
-            } if (mapaBidimensional[aux[0]][aux[1]] == 'I') {
-                if (inverter) { inverter = false; }
+            } if (mapaBidimensional[actual[0]][actual[1]] == 'I') {
+                if (inverter == true) { inverter = false; }
                 else { inverter = true; }
-                perimetro = false;
-                position = aux;
+                collision = false;
+                position = actual;
                 sb.append(movement[cont]);
                 cont = 0;
                 continue;
-            }
-
-            if (mapaBidimensional[aux[0]][aux[1]] == '$') {
+            } if (mapaBidimensional[actual[0]][actual[1]] == '$') {
                 sb.append(movement[cont]);
                 return sb.toString();
             }
